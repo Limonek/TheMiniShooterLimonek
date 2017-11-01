373fc3447c18b8d83cbf4d0cbb412b26e4a1f981
alamakota222 <jkoterwa@o2.pl>
2017-11-01 17:32:34 +0100

    dodaje projekt c++

---
+++ Source/MiniShooter/MiniShooter.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MiniShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MiniShooter, "MiniShooter" );
