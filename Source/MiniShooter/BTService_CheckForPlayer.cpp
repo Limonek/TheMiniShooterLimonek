3e2902cc4c3ff5c6bbefef174b3deccf9c72d478
alamakota222 <jkoterwa@o2.pl>
2017-11-28 02:55:32 +0100

    dzialaja boty
    dzialaja boty

---
+++ Source/MiniShooter/BTService_CheckForPlayer.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BTService_CheckForPlayer.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackBoard/BlackboardKeyAllTypes.h"
+#include "AIBasicEnemyChar.h"
+#include "AIBasicEnemyCtr.h"
+#include "MiniShooter.h"
+#include "EngineGlobals.h"
+#include "Engine/Engine.h"
+
+
+UBTService_CheckForPlayer::UBTService_CheckForPlayer()
+{
+	bCreateNodeInstance = true;
+}
+void UBTService_CheckForPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	AAIBasicEnemyCtr *EnemyPC = Cast<AAIBasicEnemyCtr>(OwnerComp.GetAIOwner());
+
+	if (EnemyPC)
+	{
+		ACharacter *Enemy = Cast<ACharacter>(GetWorld()->GetFirstPlayerController()->GetCharacter());
+		if (Enemy)
+		{
+			OwnerComp.GetBlackboardComponent()->SetValue<UBlackboardKeyType_Object>(EnemyPC->EnemyKeyID, Enemy);
+		}
+	}
+}
+
---
+++ Source/MiniShooter/BTTask_MoveToPlayer.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BTTask_MoveToPlayer.h"
+#include "MiniShooter.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackBoard/BlackboardKeyAllTypes.h"
+#include "AIBasicEnemyChar.h"
+#include "AIBasicEnemyCtr.h"
+
+EBTNodeResult::Type UBTTask_MoveToPlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIBasicEnemyCtr *CharPc = Cast<AAIBasicEnemyCtr>(OwnerComp.GetAIOwner());
+
+	ACharacter *Enemy = Cast<ACharacter>(OwnerComp.GetBlackboardComponent()->GetValue<UBlackboardKeyType_Object>(CharPc->EnemyKeyID));
+
+	if (Enemy)
+	{
+		CharPc->MoveToActor(Enemy, 5.f, true, true, true, 0, true);
+		return EBTNodeResult::Succeeded;
+	}
+	else
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	EBTNodeResult::Failed;
+}
+
